7edda88eeeb074733d559092ac6698f7cdb498a6
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-31 16:29:27 +0400

    Service

---
+++ src/main/java/com/service/DemoWebService.java
@@ -0,0 +1,13 @@
+package com.service;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface DemoWebService {
+
+    @WebMethod
+    public Double getResult(Integer a, Integer b, Integer c);
+}
