80f93c2763d5cf5a84d5e9ccc6ff00878b121429
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-31 17:12:21 +0400

    Service-1

--- src/main/java/com/WebServicePublisher.java
+++ src/main/java/com/WebServicePublisher.java
@@ -1,4 +1,11 @@
 package com;
 
+import com.service.DemoWebServiceImpl;
+
+import javax.xml.ws.Endpoint;
+
 public class WebServicePublisher {
+    public static void main(String[] args) {
+        Endpoint.publish("http://localhost:1986/wss/hello", new DemoWebServiceImpl());
+    }
 }
---
+++ src/main/java/com/service/DemoWebServiceImpl.java
@@ -0,0 +1,13 @@
+package com.service;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "com.service.DemoWebService")
+public class DemoWebServiceImpl implements DemoWebService {
+    @Override
+    public Double getResult(Integer a, Integer b, Integer c) {
+        Double d = Double.valueOf((b * b) - (4 * a * c));
+
+        return ((b * (-1) + Math.sqrt(d)) / (2 * a));
+    }
+}
